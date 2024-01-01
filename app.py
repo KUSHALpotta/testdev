d4a9a35c346dac3d1ca287eba5248caa9f8868f2
kushal potta <kushalkumar.is20@bmsce.ac.in>
2024-01-01 20:47:17 +0530

    back to sq1

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home_page(name=None):
     return render_template('index.html',name=name)
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',port=5020)
+    app.run(host='0.0.0.0',port=5000)
